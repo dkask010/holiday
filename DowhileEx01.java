8f1e9180bca2842ca21a3f47de195c4c6e5cc530
dkask010 <56530221+dkask010@users.noreply.github.com>
2019-10-14 15:00:11 +0900

    Add files via upload

---
+++ DowhileEx01.java
@@ -0,0 +1,27 @@
+class DowhileEx01 {
+	public static void main(String[] args) {
+		
+		int a = 0;
+		do{
+			System.out.println(a);
+			a++;
+		}while(a < 5);
+
+		int num = 12;
+		String str = "hello";
+		do{
+			System.out.println(str);
+		}while(num--> 10);
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
